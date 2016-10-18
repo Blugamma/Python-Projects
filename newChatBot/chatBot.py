2af2e2d44be7c4745417edb1463778b64bb40fa6
Oliver Smith <Oliver Smith>
2016-10-18 18:07:47 +0100

    Attempting tell a joke

--- newChatBot/chatBot.py
+++ newChatBot/chatBot.py
@@ -1,6 +1,6 @@
 ###ChatBot###
 import random
-
+import wikipedia
 with open('stopWords.txt', 'r') as doc:
     stopWords = doc.read().split("\n")
     stopWords.append("name")
@@ -11,6 +11,9 @@ positiveAns = ["great", "good", "not bad", "amazing", "fine thanks"]
 positiveRes = ["That's great", "Good to know", "Glad to here", "Amazing News"]
 negativeAns = ["bad", "really annoyed", "could be better", "pissed off", "not good"]
 negativeRes = ["That's a shame", "Cheer up", "Things could be worse", "Atleast you aren't a robot"]
+tellAJoke = ["tell a joke"]
+jokeRes = ["Why did the programmer quit his Job?"]
+jokeAns = ["Because he didn't get arrays."]
 random_welcoming = random.choice(welcoming)
 random_question = random.choice(questions)
 random_positiveRes = random.choice(positiveRes)
@@ -30,5 +33,7 @@ if welcoming_response.lower() in positiveAns:
     calculateInput(welcoming_response,random_positiveRes,False)
 if welcoming_response.lower() in negativeAns:
     calculateInput(welcoming_response,random_negativeRes,False)
-
-    
+    question1_response = raw_input("BluBot: How can I help?")
+    if question1_response.lower() in tellAJoke:
+        calculateInput(question1_response,jokeRes,False)
+print wikipedia.summary("Albert Einstein", sentences=2)
